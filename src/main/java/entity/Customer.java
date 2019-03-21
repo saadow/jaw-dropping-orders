ab1982abd3c83511404c95f8de66cc4846c32032
vadim <vadim.chumasov@gmail.com>
2019-03-21 17:06:03 +0200

    JsonIgnore for Salesreps

--- src/main/java/entity/Customer.java
+++ src/main/java/entity/Customer.java
@@ -2,6 +2,8 @@ package entity;
 
 import javax.persistence.*;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import java.math.BigDecimal;
 
 @Entity
@@ -11,7 +13,7 @@ public class Customer implements java.io.Serializable {
 	@Id
 	@Column(name = "CUST_NUM")
 	private BigDecimal custNum;
-	
+	@JsonIgnore
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "CUST_REP")
 	private Salesreps custRep;
@@ -58,13 +60,50 @@ public class Customer implements java.io.Serializable {
 		return company;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((company == null) ? 0 : company.hashCode());
+		result = prime * result + ((creditLimit == null) ? 0 : creditLimit.hashCode());
+		result = prime * result + ((custNum == null) ? 0 : custNum.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Customer other = (Customer) obj;
+		if (company == null) {
+			if (other.company != null)
+				return false;
+		} else if (!company.equals(other.company))
+			return false;
+		if (creditLimit == null) {
+			if (other.creditLimit != null)
+				return false;
+		} else if (!creditLimit.equals(other.creditLimit))
+			return false;
+		if (custNum == null) {
+			if (other.custNum != null)
+				return false;
+		} else if (!custNum.equals(other.custNum))
+			return false;
+		return true;
+	}
+
 	public void setCompany(String company) {
 		this.company = company;
 	}
 
 	@Override
 	public String toString() {
-		return "Customer{" + "custNum=" + custNum + ", company=" + company + ", custRep=" + custRep + ", creditLimit="
+		return "Customer{" + "custNum=" + custNum + ", company=" + company + ", creditLimit="
 				+ creditLimit + '}';
 	}
 }
