f5e5b500791831214c979f3787ac8b4a8680fc95
vadim <vadim.chumasov@gmail.com>
2019-03-25 13:21:22 +0200

    fixed requested changes: added @Override, deleted example test, removed sequence generation from schema.sql

--- src/main/java/service/CustomerServiceImpl.java
+++ src/main/java/service/CustomerServiceImpl.java
@@ -2,18 +2,14 @@ package service;
 
 import java.math.BigDecimal;
 import java.util.HashSet;
-import java.util.NoSuchElementException;
-import java.util.Optional;
 import java.util.Set;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import entity.Customer;
-import exception.DeleteException;
 import repository.CustomerRepository;
 
 @Service
@@ -24,31 +20,36 @@ public class CustomerServiceImpl implements CustomerService {
 	@Autowired
 	private CustomerRepository customerRepository;
 
+	@Override
 	public Set<Customer> getAllCustomers() {
 		LOG.debug("get all customers");
 		HashSet<Customer> result = new HashSet<Customer>(customerRepository.findAll());
 		LOG.debug("{} results", result.size());
 		return result;
 	}
-
+	
+	@Override
 	public void insertCustomer(Customer customer) {
 		LOG.debug("insert customer = {}", customer);
 		customerRepository.save(customer);
 		LOG.debug("insert completed");
 	}
 
+	@Override
 	public void updateCustomer(Customer customer) {
 		LOG.debug("update customer={}", customer);
 		customerRepository.save(customer);
 		LOG.debug("update completed");
 	}
-
+	
+	@Override
 	public void deleteCustomer(BigDecimal custNum) {
 		LOG.debug("delete customer by CustNum = {}", custNum);
 		customerRepository.deleteById(custNum);
 		LOG.debug("delete customer completed");
 	}
 	
+	@Override
 	public Customer findCustomerById(BigDecimal id) {
 		LOG.debug("Find Customer by id={}", id);
 		Customer result = customerRepository.findById(id).orElse(null);
